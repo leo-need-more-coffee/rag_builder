d2dedb6e411344ea66c59a8ae7400aa27d8d5499
лень <lazy@pzrnqt1vrss.xyz>
2024-10-29 23:23:49 +0300

    edit setup

--- setup.py
+++ setup.py
@@ -3,14 +3,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="llm-rag-builder",
-    version="0.1.0",
+    version="0.1.4",
     description="Это библиотека на Python, предназначенная для упрощения создания и управления моделями генерации с использованием поиска (Retrieval-Augmented Generation, RAG).",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="лень",
     author_email="pzrnqt1vrss@protonmail.com",
     url="https://github.com/leo-need-more-coffee/rag_builder",
-    packages=find_packages(),
+    packages=[
+        'rag_builder',
+        'rag_builder.base',
+        'rag_builder.integrations',
+        'rag_builder.integrations.dialogs',
+        'rag_builder.integrations.llms',
+        'rag_builder.integrations.vector_databases',
+        'rag_builder.integrations.vectorizers',
+        'rag_builder.utils'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
